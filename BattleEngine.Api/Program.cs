f341f2216ac2149b30b161627d8a7ca42b89fa4c
Pierre "Golluméo" Mauriello <hire-me@pierre-mauriello.be>
2025-11-24 16:29:47 +0100

    chore(git): normalize EOL & merge strats with .gitattributes

---
+++ BattleEngine.Api/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+// dummy test route
+app.MapGet("/", () => Results.Json(new { hello = "mékoyes" }));
+app.Run();
